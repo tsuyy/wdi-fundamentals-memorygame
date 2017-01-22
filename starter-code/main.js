c56d208297164741fd3c641660ab71d172cdfd4b
YOUR_GITHUB_USERNAME <yvonnetsu@Yvonnes-MacBook-Air.local>
2017-01-22 13:10:54 -0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,41 @@
-console.log("JS file is connected to HTML! Woo!")
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+}
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='https://s-media-cache-ak0.pinimg.com/originals/dd/17/cb/dd17cb0f0e436fa60ace35306478a32d.png'>"; // king
+	} else {
+		this.innerHTML = "<img src='https://upload.wikimedia.org/wikipedia/commons/1/1e/Poker-sm-243-Qc.png'>"; //queen
+	}
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
